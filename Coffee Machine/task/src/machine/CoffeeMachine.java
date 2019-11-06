fe7b1e1c87e44d1bb030e6d2cbad2f76129b68d2
Jerami <jeramilessard@gmail.com>
2019-11-06 14:37:55 -0500

    Update if/else in buyCoffee() to guard clauses Add String fillPrompt[] Remove enum FillPromptState

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -3,6 +3,7 @@ package machine;
 import java.util.Scanner;
 
 public class CoffeeMachine {
+
     private Scanner userInput;
 
     private int water;
@@ -12,53 +13,34 @@ public class CoffeeMachine {
     private int cash;
     private boolean powerOn;
 
+
     private CoffeeMachine(){
         this.water = 400;
         this.beans = 120;
         this.milk = 540;
-        this.cups = 4;
+        this.cups = 9;
         this.cash = 550;
         this.powerOn = true;
         this.userInput =new Scanner(System.in);
     }
 
-    public enum FillPromptState {  //Should probably change this to a String[]
-        WATER_FILL_PROMPT("ml of water"),
-        MILK_FILL_PROMPT("ml of milk"),
-        BEANS_FILL_PROMPT("grams of coffee beans"),
-        CUPS_FILL_PROMPT("cups");
-        String fillPromptVar;
-        FillPromptState (String fillPromptVar) {
-            this.fillPromptVar = fillPromptVar;
-        }
-    }
-
-    //Should implement MachineState next
-//    public enum MachineState {
-//        STANDBY,
-//        BUY,
-//        BREW,
-//        FILL,
-//    }
-
-
-    private boolean userInteract(){
+    private boolean userInteract(){ //All user input handled here
         System.out.print("Write action (buy, fill, take, remaining, exit): ");
         String command = userInput.next();
         switch (command) {
             case "buy":
                 System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ");
-                command = userInput.next();
-                if(command.equals("back")){
+                if(userInput.hasNextInt()){
+                    int coffeeType = userInput.nextInt();
+                    buyCoffee(coffeeType);
                     break;
                 }
-                buyCoffee(command);
                 break;
             case "fill":
-                FillPromptState[] fillPromptState = FillPromptState.values();
+                String[] fillPrompts = new String[]{"ml of water", "ml of milk", "grams of coffee beans", "cups"};
                 int[] fillAmounts = new int[4];
                 for(int i=0; i <4; i++){
-                    System.out.printf("Write how many %s do you want to add: ",fillPromptState[i].fillPromptVar);
+                    System.out.printf("Write how many %s do you want to add: ",fillPrompts[i]);
                     fillAmounts[i] = userInput.nextInt();
                 }
                 updateSupplies(fillAmounts[0],fillAmounts[1],fillAmounts[2],fillAmounts[3]);
@@ -70,69 +52,72 @@ public class CoffeeMachine {
             case "remaining":
                 printStatus();
                 break;
-            case "exit":
+            case "exit": //close scanner and set powerOn to false
                 userInput.close();
                 return false;
         }
-        return true;
+        return true; //Continue running userInteract()
     }
 
     private void printStatus() {
         System.out.printf("%nThe coffee machine has:%n%d of water%n%d of milk%n%d of coffee beans%n%d of disposable cups%n%d of money%n%n",water, milk, beans, cups, cash);
     }
 
-    private void buyCoffee(String coffeeType) {
+    private void buyCoffee(int coffeeType) {
         int waterReqd = 0;
         int milkReqd = 0;
         int beansReqd = 0;
-        int price = 0;
         int cupsReqd = 1;
+        int price = 0;
         switch(coffeeType){
-            case "1"://espresso
+            case 1://espresso
                 waterReqd = 250;
                 beansReqd = 16;
                 price = 4;
                 break;
-            case "2"://latte
+            case 2://latte
                 waterReqd= 350;
                 milkReqd= 75;
                 beansReqd= 20;
                 price = 7;
                 break;
-            case "3"://cappuccino
+            case 3://cappuccino
                 waterReqd= 200;
                 milkReqd= 100;
                 beansReqd= 12;
                 price = 6;
                 break;
-            case "4":
-                cupsReqd = 4;
-                break;
-            case "back":
-                return;
         }
+        //Guard clauses instead of if else
         if(waterReqd>water) {
             System.out.println("Sorry, not enough water!");
-        } else if (milkReqd>milk) {
-            System.out.println("Sorry, not enough milk!");
-        }  else if (beansReqd>beans) {
-            System.out.println("Sorry, not enough beans!");
-        } else if (this.cups<1) {
-            System.out.println("Sorry, not enough cups!");
-        } else {
-            System.out.println("I have enough resources, making you a coffee!");
-            updateSupplies(waterReqd,milkReqd,beansReqd,cupsReqd,price);
+            return;
         }
+        if (milkReqd>milk) {
+            System.out.println("Sorry, not enough milk!");
+            return;
+        }
+        if (beansReqd>beans) {
+            System.out.println("Sorry, not enough beans!");
+            return;
+        }
+        if (this.cups<1) {
+            System.out.println("Sorry, not enough cups!");
+            return;
+        }
+        System.out.println("I have enough resources, making you a coffee!");
+        updateSupplies(waterReqd, milkReqd, beansReqd, cupsReqd, price);
+
     }
 
     /* updateSupplies() will handle all variable updates and is overloaded to be reused depending on the number of arguments sent */
-    private void updateSupplies(int water,int milk,int beans,int cups) {
+    private void updateSupplies(int water, int milk, int beans, int cups) {
         this.water += water;
         this.milk += milk;
         this.beans += beans;
         this.cups += cups;
     }
-    private void updateSupplies(int water,int milk,int beans,int cups, int cash) {
+    private void updateSupplies(int water, int milk, int beans, int cups, int cash) {
         this.water -= water;
         this.milk -= milk;
         this.beans -= beans;
@@ -145,7 +130,7 @@ public class CoffeeMachine {
 
     public static void main(String[] args) {
         CoffeeMachine breakRoomMachine = new CoffeeMachine();
-        CoffeeMachine testMachine = new CoffeeMachine();
+        CoffeeMachine testMachine = new CoffeeMachine();//This object is here to compare variables for testing purposes
        do {
             breakRoomMachine.powerOn = breakRoomMachine.userInteract();
             }  while(breakRoomMachine.powerOn);
